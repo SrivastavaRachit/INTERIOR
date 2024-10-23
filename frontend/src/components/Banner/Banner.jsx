75745a4aa441f70e364c67b3e9b81ed78eb843a3
SrivastavaRachit <rachitsrivastava@gmail.com>
2024-10-24 01:50:41 +0530

    Banner

--- frontend/src/components/Banner/Banner.jsx
+++ frontend/src/components/Banner/Banner.jsx
@@ -4,17 +4,18 @@ import { motion } from 'framer-motion';
 const Banner = () => {
     return (
         <section
-            className="relative w-full h-96 md:h-[500px] lg:h-[600px] bg-fixed bg-cover bg-center"
+            className="relative w-full h-[60vh] sm:h-[50vh] md:h-[60vh] lg:h-[70vh] bg-fixed bg-cover bg-center"
             style={{
-                backgroundImage: `url('https://img.freepik.com/free-photo/christmas-celebration-with-decorated-tree_23-2151834376.jpg?uid=R167672705&ga=GA1.1.1165456550.1728109905')`,
+                backgroundImage: `url('https://img.freepik.com/free-photo/3d-rendering-loft-luxury-living-room-with-shelf-near-dining-table-counter_105762-2105.jpg?t=st=1729714480~exp=1729718080~hmac=74dc905cf91cace17c40efbca7b8394c849526daaafd96879de4685c53e4131e&w=1380')`,
             }}
         >
             {/* Gradient Overlay */}
             <div className="absolute inset-0 bg-gradient-to-b from-black via-transparent to-black opacity-70"></div>
+
             {/* Banner Content */}
-            <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-4 md:px-8 lg:px-16">
+            <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-4 sm:px-6 md:px-8 lg:px-12 xl:px-16">
                 <motion.h1
-                    className="text-white text-4xl md:text-5xl lg:text-6xl font-extrabold tracking-wide mb-4"
+                    className="text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl font-extrabold tracking-wider mb-4"
                     initial={{ opacity: 0, y: -50 }}
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 1, ease: 'easeOut' }}
--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faBlog, faProjectDiagram, faInfoCircle, faPhone, faEnvelope, faChevronUp, faBuilding, faCouch, faPeopleArrows } from '@fortawesome/free-solid-svg-icons';
+import {
+  faHome, faBlog, faProjectDiagram, faInfoCircle, faPhone, faEnvelope, faChevronUp, faBuilding, faCouch, faPeopleArrows
+} from '@fortawesome/free-solid-svg-icons';
 import { faFacebookF, faTwitter, faInstagram, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 import { NavLink } from 'react-router-dom';
-
 
 const Footer = () => {
   return (
@@ -20,30 +21,26 @@ const Footer = () => {
         {/* Services Section */}
         <div className="w-full lg:w-1/4 space-y-3">
           <h4 className="text-xl font-semibold text-gradient mb-3">Services</h4>
-          <ul className="space-y-2 ">
+          <ul className="space-y-2">
             <li>
               <NavLink
                 to="/service/home-design"
-                className="text-gray-400 hover:text-purple-300 transition duration-300 hover:underline flex items-center"
-              >
-                <FontAwesomeIcon icon={faHome} className="mr-2" />
-                Home Design
+                className="text-gray-400 hover:text-purple-300 transition  duration-300 hover:underline flex items-center">
+                <FontAwesomeIcon icon={faHome} className="mr-2"/><span className='ml-1'>Home Design</span>
               </NavLink>
             </li>
             <li>
               <NavLink
                 to="/service/office-design"
-                className="text-gray-400 hover:text-purple-300 transition duration-300 hover:underline flex items-center"
-              >
-                <FontAwesomeIcon icon={faBuilding} className="mr-2" />
+                className="text-gray-400 hover:text-purple-300 transition duration-300 hover:underline flex items-center">
+                <FontAwesomeIcon icon={faBuilding} className="mr-3 ml-1" />
                 Office Design
               </NavLink>
             </li>
             <li>
               <NavLink
                 to="/service/custom-furniture"
-                className="text-gray-400 hover:text-purple-300 transition duration-300 hover:underline flex items-center"
-              >
+                className="text-gray-400 hover:text-purple-300 transition duration-300 hover:underline flex items-center">
                 <FontAwesomeIcon icon={faCouch} className="mr-2" />
                 Custom Furniture
               </NavLink>
@@ -51,8 +48,7 @@ const Footer = () => {
             <li>
               <NavLink
                 to="/service/consultations"
-                className="text-gray-400 hover:text-purple-300 transition duration-300 hover:underline flex items-center"
-              >
+                className="text-gray-400 hover:text-purple-300 transition duration-300 hover:underline flex items-center">
                 <FontAwesomeIcon icon={faPeopleArrows} className="mr-2" />
                 Consultations
               </NavLink>
@@ -65,11 +61,11 @@ const Footer = () => {
           <h4 className="text-xl font-semibold text-gradient mb-3">Get In Touch</h4>
           <p className="text-gray-400 flex items-center">
             <FontAwesomeIcon icon={faEnvelope} className="mr-2" />
-            Email: hexagonsservices@gmail.com
+            <a href="mailto:hexagonsservices@gmail.com" className="hover:underline">Email: hexagonsservices@gmail.com</a>
           </p>
           <p className="text-gray-400 flex items-center">
             <FontAwesomeIcon icon={faPhone} className="mr-2" />
-            Phone: (+91 8840775386)
+            <a href="tel:+918840775386" className="hover:underline">Phone: +91 8840775386</a>
           </p>
           {/* Social Media Links */}
           <div className="flex space-x-4 mt-6">
@@ -88,13 +84,12 @@ const Footer = () => {
               </a>
             ))}
           </div>
-
         </div>
 
         {/* Quick Links Section */}
         <div className="w-full lg:w-1/4 space-y-3">
           <h4 className="text-xl font-semibold text-gradient mb-3">Quick Links</h4>
-          <ul className="space-y-2 ">
+          <ul className="space-y-2">
             <li>
               <NavLink
                 to="/"
@@ -164,7 +159,7 @@ const Footer = () => {
       <div className="border-t border-gray-700 mt-10 pt-6">
         <p className="text-center  text-gray-500 text-lg">&copy; 2024 Interior Impression.
           <p className="text-center text-gray-500 text-lg"> All Rights Reserved.</p>
-          <a href="https://hexagondigitalservices.com/" target='blank'>Powered By {" "}
+          <a href="https://hexagondigitalservices.com/" target='_blank' rel="noreferrer">Powered By {" "}
             <span
               className='text-yellow-400 hover:text-blue-300 underline'>
               Hexagon Digital Services
